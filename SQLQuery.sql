b2988a3cb1324a0105621be936d192e169856ab1
WitaminF <scheiblerf@gmail.com>
2022-09-22 14:57:14 +0200

    Inserer et selectionner les données dans une table

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -12,7 +12,20 @@ CREATE DATABASE BDD
 USE BDD
 CREATE TABLE Personne (nom varchar(200), prenom varchar(200))
 
+-- Renomer une table
 sp_rename 'Personne', 'Citoyen'
 
+-- Supprimer une table
 DROP TABLE Citoyen
+
+-- Insérer des données dans la table (INTO n'est pas obligatoire)
+INSERT INTO Citoyen VALUES
+('Le roi', 'Lion'),
+('Winnnie', 'L''ourson')
+
+-- Selectionner des données dans une table
+SELECT * FROM Citoyen
+
+-- Selectionner des données dans une table avec WHERE
+SELECT * FROM Citoyen WHERE prenom = 'Lion'
 
